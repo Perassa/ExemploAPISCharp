c561bbc87aa4191cea4c8e16d642f9617e9ac18c
Perassa <129907619+Perassa@users.noreply.github.com>
2024-02-02 22:16:50 -0300

    criando classe pessoa

---
+++ ExemploAPISCharp/ExemploAPISCharp/Controllers/PessoaController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ExemploAPISCharp.Model;
+
+namespace ExemploAPISCharp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PessoaController : ControllerBase
+    {
+        [HttpGet("GetPessoa")]
+        public Pessoa GetPessoa()
+        {
+            var vPessoa = new Pessoa();
+            vPessoa.id = 1;
+            vPessoa.nome = "josé das couves";
+            vPessoa.NumeroCelular = "(18) 98569-7845";
+
+                return vPessoa;
+        }
+    }
+}
---
+++ ExemploAPISCharp/ExemploAPISCharp/Model/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿namespace ExemploAPISCharp.Model
+{
+    public class Pessoa
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+
+        public string NumeroCelular { get; set; }
+
+        public DateOnly  DataNascimento{ get; set; }
+
+
+    }
+}
